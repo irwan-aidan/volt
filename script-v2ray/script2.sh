1805a63448538679872ecc7256655410823dbee6
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-08 06:07:07 +0800

    Create script2.sh

---
+++ script-v2ray/script2.sh
@@ -0,0 +1,40 @@
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/di2nk/v2/main/menu.sh"
+wget -O addws "https://raw.githubusercontent.com/di2nk/v2/main/addws.sh"
+wget -O addvless "https://raw.githubusercontent.com/di2nk/v2/main/addvless.sh"
+wget -O addtr "https://raw.githubusercontent.com/di2nk/v2/main/addtr.sh"
+wget -O delws "https://raw.githubusercontent.com/di2nk/v2/main/delws.sh"
+wget -O delvless "https://raw.githubusercontent.com/di2nk/v2/main/delvless.sh"
+wget -O deltr "https://raw.githubusercontent.com/di2nk/v2/main/deltr.sh"
+wget -O cekws "https://raw.githubusercontent.com/di2nk/v2/main/cekws.sh"
+wget -O cekvless "https://raw.githubusercontent.com/di2nk/v2/main/cekvless.sh"
+wget -O cektr "https://raw.githubusercontent.com/di2nk/v2/main/cektr.sh"
+wget -O renewws "https://raw.githubusercontent.com/di2nk/v2/main/renewws.sh"
+wget -O renewvless "https://raw.githubusercontent.com/di2nk/v2/main/renewvless.sh"
+wget -O renewtr "https://raw.githubusercontent.com/di2nk/v2/main/renewtr.sh"
+wget -O xp-ws "https://raw.githubusercontent.com/di2nk/v2/main/xp-ws.sh"
+wget -O xp-tr "https://raw.githubusercontent.com/di2nk/v2/main/xp-tr.sh"
+wget -O xp-vless "https://raw.githubusercontent.com/di2nk/v2/main/xp-vless.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/di2nk/v2/main/cert.sh"
+wget -O about "https://raw.githubusercontent.com/di2nk/v2/main/about.sh"
+wget -O addhost "https://raw.githubusercontent.com/di2nk/v2/main/addhost.sh"
+chmod +x menu
+chmod +x addws
+chmod +x addvless
+chmod +x addtr
+chmod +x delws
+chmod +x delvless
+chmod +x deltr
+chmod +x cekws
+chmod +x cekvless
+chmod +x cektr
+chmod +x renewws
+chmod +x renewtr
+chmod +x renewvless
+chmod +x xp-ws
+chmod +x xp-tr
+chmod +x xp-vless
+chmod +x certv2ray
+chmod +x about
+chmod +x addhost
+cd
