271d5cbd4561bfecb9529a2142309ae237f129ed
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-12 05:37:18 +0800

    Update install-xray.sh

--- modul/install-xray.sh
+++ modul/install-xray.sh
@@ -2,14 +2,14 @@
 #!/bin/bash
 domain=$(cat /root/domain)
 apt install iptables iptables-persistent -y
-apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install curl socat xz-utils wget apt-transport-https nginx gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
 ntpdate pool.ntp.org
 apt -y install chrony
 timedatectl set-ntp true
 systemctl enable chronyd && systemctl restart chronyd
 systemctl enable chrony && systemctl restart chrony
-timedatectl set-timezone Asia/Jakarta
+timedatectl set-timezone Asia/Kuala_Lumpur
 chronyc sourcestats -v
 chronyc tracking -v
 date
