9a02c7f5591af8ba4bc47309bbe2d9719c37936e
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-12 05:24:56 +0800

    Update deb10.sh

--- modul/deb10.sh
+++ modul/deb10.sh
@@ -1108,8 +1108,6 @@ apt-get -y remove sendmail*
 apt autoremove -y
 # finishing
 cd
-chown -R www-data:www-data /home/vps/public_html
-/etc/init.d/nginx restart
 /etc/init.d/openvpn restart
 /etc/init.d/cron restart
 /etc/init.d/ssh restart
