0df10da9465d889231144dc27a7a39301be6cd4f
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-12 03:51:17 +0800

    Update menu-xray.sh

--- script2/menu-xray.sh
+++ script2/menu-xray.sh
@@ -5,7 +5,7 @@ function add-user() {
 	echo -e "Add Xray User"
 	echo -e "-------------"
 	read -p "Username : " user
-	if grep -qw "$user" /donb/xray/xray-clients.txt; then
+	if grep -qw "$user" /dani/xray/xray-clients.txt; then
 		echo -e ""
 		echo -e "User '$user' already exist."
 		echo -e ""
@@ -32,16 +32,16 @@ function add-user() {
 	echo -e "-------------------------------"
 	echo -e "Host : $domain"
 	echo -e "Username : $user"
-	echo -e "Telco : Umobile / Digi"
+	echo -e "Type : Direct, Splice"
 	echo -e "Validity : day"
 	echo -e "Expired date : $expired"
 	echo -e "-------------------------------"
 	echo -e "XRAY Config Information"
 	echo -e ""
 	echo -e ""
-	echo -e "Link Digi    : vless://$uuid@vault21.digi.com.my.$domain:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-splice&security=xtls&sni=vault21.digi.com.my#vless_xtls_Digi_$user"
+	echo -e "Link Direct : vless://$uuid@$domain:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=$domain#vless_xtls_Direct_$user"
 	echo -e ""
-	echo -e "Link Umobile : vless://$uuid@music.u.com.my.$domain:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-splice&security=xtls&sni=clubopen.pubgmobile.com.music.u.com.my#vless_xtls_Umobile_$user"
+	echo -e "Link Splice : vless://$uuid@$domain:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-splice&security=xtls&sni=$domain#vless_xtls_Splice_$user"
 	
 }
 
@@ -51,13 +51,13 @@ function delete-user() {
 	echo -e "----------------"
 	read -p "Username : " user
 	echo -e ""
-	if ! grep -qw "$user" /donb/xray/xray-clients.txt; then
+	if ! grep -qw "$user" /dani/xray/xray-clients.txt; then
 		echo -e ""
 		echo -e "User '$user' does not exist."
 		echo -e ""
 		exit 0
 	fi
-	uuid="$(cat /donb/xray/xray-clients.txt | grep -w "$user" | awk '{print $2}')"
+	uuid="$(cat /dani/xray/xray-clients.txt | grep -w "$user" | awk '{print $2}')"
 
 	cat /usr/local/etc/xray/config.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /usr/local/etc/xray/config_tmp.json
 	mv -f /usr/local/etc/xray/config_tmp.json /usr/local/etc/xray/config.json
@@ -75,7 +75,7 @@ function extend-user() {
 	echo -e "Extend Xray User"
 	echo -e "----------------"
 	read -p "Username : " user
-	if ! grep -qw "$user" /donb/xray/xray-clients.txt; then
+	if ! grep -qw "$user" /dani/xray/xray-clients.txt; then
 		echo -e ""
 		echo -e "User '$user' does not exist."
 		echo -e ""
@@ -83,15 +83,15 @@ function extend-user() {
 	fi
 	read -p "Duration (day) : " extend
 
-	uuid=$(cat /donb/xray/xray-clients.txt | grep -w $user | awk '{print $2}')
-	exp_old=$(cat /donb/xray/xray-clients.txt | grep -w $user | awk '{print $3}')
+	uuid=$(cat /dani/xray/xray-clients.txt | grep -w $user | awk '{print $2}')
+	exp_old=$(cat /dani/xray/xray-clients.txt | grep -w $user | awk '{print $3}')
 	diff=$((($(date -d "${exp_old}" +%s)-$(date +%s))/(86400)))
 	duration=$(expr $diff + $extend + 1)
 	exp_new=$(date -d +${duration}days +%Y-%m-%d)
 	exp=$(date -d "${exp_new}" +"%d %b %Y")
 
-	sed -i "/\b$user\b/d" /donb/xray/xray-clients.txt
-	echo -e "$user\t$uuid\t$exp_new" >> /donb/xray/xray-clients.txt
+	sed -i "/\b$user\b/d" /dani/xray/xray-clients.txt
+	echo -e "$user\t$uuid\t$exp_new" >> /dani/xray/xray-clients.txt
 
 	clear
 	echo -e "Success Extend Xray User Information"
@@ -116,8 +116,8 @@ function user-list() {
 		exp=$(echo $expired | awk '{print $3}')
 		exp_date=$(date -d"${exp}" "+%d %b %Y")
 		printf "%-17s %2s\n" "$user" "$exp_date"
-	done < /donb/xray/xray-clients.txt
-	total=$(wc -l /donb/xray/xray-clients.txt | awk '{print $1}')
+	done < /dani/xray/xray-clients.txt
+	total=$(wc -l /dani/xray/xray-clients.txt | awk '{print $1}')
 	echo -e "-------------------------------"
 	echo -e "Total accounts: $total"
 	echo -e "==============================="
@@ -125,7 +125,7 @@ function user-list() {
 }
 
 function user-monitor() {
-	data=($(cat /donb/xray/xray-clients.txt | awk '{print $1}'))
+	data=($(cat /dani/xray/xray-clients.txt | awk '{print $1}'))
 	data2=($(netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | grep -w 443 | awk '{print $5}' | cut -d: -f1 | sort | uniq))
 	domain=$(cat /usr/local/etc/xray/domain)
 	clear
@@ -156,7 +156,7 @@ function user-monitor() {
 }
 
 function user-monitor() {
-	data=($(cat /donb/xray/xray-clients.txt | awk '{print $1}'))
+	data=($(cat /dani/xray/xray-clients.txt | awk '{print $1}'))
 	data2=($(netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | grep -w 443 | awk '{print $5}' | cut -d: -f1 | sort | uniq))
 	domain=$(cat /usr/local/etc/xray/domain)
 	clear
@@ -190,15 +190,15 @@ function show-config() {
 	echo -e "Xray Config"
 	echo -e "-----------"
 	read -p "User : " user
-	if ! grep -qw "$user" /donb/xray/xray-clients.txt; then
+	if ! grep -qw "$user" /dani/xray/xray-clients.txt; then
 		echo -e ""
 		echo -e "User '$user' does not exist."
 		echo -e ""
 		exit 0
 	fi
-	uuid=$(cat /donb/xray/xray-clients.txt | grep -w "$user" | awk '{print $2}')
+	uuid=$(cat /dani/xray/xray-clients.txt | grep -w "$user" | awk '{print $2}')
 	domain=$(cat /usr/local/etc/xray/domain)
-	exp=$(cat /donb/xray/xray-clients.txt | grep -w "$user" | awk '{print $3}')
+	exp=$(cat /dani/xray/xray-clients.txt | grep -w "$user" | awk '{print $3}')
 	exp_date=$(date -d"${exp}" "+%d %b %Y")
 
 	echo -e "Expired : $exp_date"
@@ -232,9 +232,9 @@ function show-config() {
 	echo -e ""
 	echo -e "QR : https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://$uuid@$domain:443?security=xtls%26encryption=none%26flow=xtls-rprx-direct%23XRAY_XTLS-$user"
 	echo -e ""
-	echo -e "Link Digi    : vless://$uuid@vault21.digi.com.my.$domain:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-splice&security=xtls&sni=vault21.digi.com.my#vless_xtls_Digi_$user"
+	echo -e "Link Direct : vless://$uuid@$domain:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-direct&security=xtls&sni=$domain#vless_xtls_Direct_$user"
 	echo -e ""
-	echo -e "Link Umobile : vless://$uuid@music.u.com.my.$domain:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-splice&security=xtls&sni=clubopen.pubgmobile.com.music.u.com.my#vless_xtls_Umobile_$user"
+	echo -e "Link Splice : vless://$uuid@$domain:443?headerType=none&type=tcp&encryption=none&flow=xtls-rprx-splice&security=xtls&sni=$domain#vless_xtls_Splice_$user"
 }
 
 clear
@@ -245,10 +245,9 @@ echo -e "  [2] Delete Xray user"
 echo -e "  [3] Extend Xray user"
 echo -e "  [4] Xray user list"
 echo -e "  [5] Xray user monitor"
-echo -e "  [6] Show Xray configuration"
-echo -e "  [7] Exit"
+echo -e "  [x] Exit"
 echo -e ""
-until [[ ${option} =~ ^[1-7]$ ]]; do
+until [[ ${option} =~ ^[1-5]$ ]]; do
 	read -rp "Select an option [1-7]: " option
 done
 case "${option}" in
@@ -271,7 +270,7 @@ case "${option}" in
 	clear
 	show-config
 	;;
-7)
+x)
 	clear
 	exit 0
 	;;
