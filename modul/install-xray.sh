e749068a73f0824a6385c98ed69a243c91a4753f
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-12 04:20:00 +0800

    Update install-xray.sh

--- modul/install-xray.sh
+++ modul/install-xray.sh
@@ -32,7 +32,7 @@ systemctl enable xray
 systemctl restart xray
 
 #MENU XRAY
-wget -qO /usr/bin/menu-xray "https://raw.githubusercontent.com/kor8/volt/beta/script2/menu-xray.sh"
-chmod +x /usr/bin/menu-xray
+wget -qO /usr/local/menu-xray "https://raw.githubusercontent.com/kor8/volt/beta/script2/menu-xray.sh"
+chmod +x /usr/local/menu-xray
 
 
