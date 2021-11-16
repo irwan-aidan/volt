bd17dcfdc3f206f58adfec627b9835bf0eab1c5a
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-17 05:47:56 +0800

    Create xray.sh

---
+++ modul/xray.sh
@@ -0,0 +1,29 @@
+apt-get install -y lsb-release gnupg2 wget lsof tar unzip curl libpcre3 libpcre3-dev zlib1g-dev openssl libssl-dev jq nginx uuid-runtime
+curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh | bash -s -- install
+echo $domain > /usr/local/etc/xray/domain
+wget -qO /usr/local/etc/xray/config.json "https://raw.githubusercontent.com/iriszz-official/autoscript/main/FILES/xray/xray.json"
+wget -qO /etc/nginx/conf.d/${domain}.conf "https://raw.githubusercontent.com/iriszz-official/autoscript/main/FILES/xray/web.conf"
+sed -i "s/xxx/${domain}/g" /etc/nginx/conf.d/${domain}.conf
+wget -qO web.tar.gz "https://raw.githubusercontent.com/iriszz-official/autoscript/main/FILES/web.tar.gz"
+rm -rf /var/www/html/*
+tar xzf web.tar.gz -C /var/www/html
+rm -f web.tar.gz
+mkdir /iriszz/xray
+curl -L get.acme.sh | bash
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+sed -i "6s/^/#/" /etc/nginx/conf.d/${domain}.conf
+sed -i "6a\\\troot /var/www/html/;" /etc/nginx/conf.d/${domain}.conf
+systemctl restart nginx
+/root/.acme.sh/acme.sh --issue -d "${domain}" --webroot "/var/www/html/" -k ec-256 --force
+/root/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /iriszz/xray/xray.crt --keypath /iriszz/xray/xray.key --reloadcmd "systemctl restart xray" --ecc --force
+sed -i "7d" /etc/nginx/conf.d/${domain}.conf
+sed -i "6s/#//" /etc/nginx/conf.d/${domain}.conf
+chown -R nobody.nogroup /iriszz/xray/xray.crt
+chown -R nobody.nogroup /iriszz/xray/xray.key
+touch /iriszz/xray/xray-clients.txt
+sed -i "s/\tinclude \/etc\/nginx\/sites-enabled\/\*;/\t# include \/etc\/nginx\/sites-enabled\/\*;asd/g" /etc/nginx/nginx.conf
+mkdir /etc/systemd/system/nginx.service.d
+printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" | tee /etc/systemd/system/nginx.service.d/override.conf
+systemctl daemon-reload
+systemctl restart nginx
+systemctl restart xray
