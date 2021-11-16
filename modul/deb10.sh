0c1bf30dd486c8ce10e31bfd8e48ebb6a2ceff65
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-17 06:38:19 +0800

    Update deb10.sh

--- modul/deb10.sh
+++ modul/deb10.sh
@@ -1080,7 +1080,7 @@ rm -rf /root/vnstat-2.6
 echo "12 0 * * * root reboot" >> /etc/crontab
 echo "0 0 * * * root xp" >> /etc/crontab
 
-cd /usr/local/sbin/
+cd /usr/bin/
 rm -rf {menu-ssh,base-ports,base-ports-wc,base-script,bench-network,clearcache,connections,create,create_random,create_trial,delete_expired,delete_all,diagnose,edit_dropbear,edit_openssh,edit_openvpn,edit_ports,edit_squid3,edit_stunnel4,locked_list,menu,options,ram,reboot_sys,reboot_sys_auto,restart_services,server,set_multilogin_autokill,set_multilogin_autokill_lib,show_ports,speedtest,user_delete,user_details,user_details_lib,user_extend,user_list,user_lock,user_unlock}
 wget -q 'https://github.com/kor8/volt/raw/beta/script/fixed1.zip'
 unzip -qq fixed1.zip
