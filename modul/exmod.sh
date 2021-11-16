79a189173ae38a29d3805a8bb6fbfc2601e3296e
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-17 05:53:24 +0800

    Create exmod.sh

---
+++ modul/exmod.sh
@@ -0,0 +1,360 @@
+#!/bin/bash
+
+# Check root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root!"
+   exit 1
+fi
+
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+   echo "OpenVZ is not supported!"
+   exit 1
+fi
+
+# Get domain
+clear
+echo -e ""
+read -p "Please enter your domain : " domain
+echo -e ""
+ip=$(wget -qO- ipv4.icanhazip.com)
+domain_ip=$(ping "${domain}" -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
+if [[ ${domain_ip} == "${ip}" ]]; then
+	echo -e "IP matched with the server. The installation will continue."
+	sleep 2
+	clear
+else
+	echo -e "IP does not match with the server. Make sure to point A record to your server."
+	echo -e ""
+	exit 1
+fi
+
+# Update & Upgrade
+apt update
+apt upgrade -y
+
+# Remove unused dependencies
+apt autoremove -y
+
+# Set timezone
+ln -sf /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+
+cat << xxx >/etc/apt/sources.list
+deb http://cloudfront.debian.net/debian buster main contrib non-free
+deb http://cloudfront.debian.net/debian buster-updates main contrib non-free
+deb http://cloudfront.debian.net/debian buster-backports main contrib non-free
+xxx
+
+# Disable IPv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+echo -e "net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+# Install BBR+FQ
+echo -e "net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+
+# Removing some firewall tools that may affect other services
+apt-get remove --purge ufw firewalld -y
+apt-get remove --purge exim4 -y
+
+# setting vnstat
+apt -y install vnstat
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev
+wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
+
+# Install tools
+apt install -y net-tools unzip curl screen neofetch
+
+# Install screenfetch
+chmod +x /usr/bin/screenfetch
+echo "clear" >> .profile
+echo 'echo '' > /var/log/syslog' >> .profile
+echo "neofetch -p -A Android" >> .profile
+echo "echo '  DARKNET PREM SCRIPT '" >> .profile
+echo "echo '  t.me/cyberbossz '" >> .profile
+
+# Configure SSH
+# Removing some duplicated sshd server configs
+rm -f /etc/ssh/sshd_config*
+ 
+# Creating a SSH server config using cat eof tricks
+cat <<'MySSHConfig' > /etc/ssh/sshd_config
+# My OpenSSH Server config
+Port 22
+Port 225
+AddressFamily inet
+ListenAddress 0.0.0.0
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostKey /etc/ssh/ssh_host_ed25519_key
+PermitRootLogin yes
+MaxSessions 1024
+PubkeyAuthentication yes
+PasswordAuthentication yes
+PermitEmptyPasswords no
+ChallengeResponseAuthentication no
+UsePAM yes
+X11Forwarding yes
+PrintMotd no
+ClientAliveInterval 240
+ClientAliveCountMax 2
+UseDNS no
+Banner /etc/banner
+AcceptEnv LANG LC_*
+Subsystem   sftp  /usr/lib/openssh/sftp-server
+MySSHConfig
+
+# My workaround code to remove `BAD Password error` from passwd command, it will fix password-related error on their ssh accounts.
+sed -i '/password\s*requisite\s*pam_cracklib.s.*/d' /etc/pam.d/common-password
+sed -i 's/use_authtok //g' /etc/pam.d/common-password
+
+# Some command to identify null shells when you tunnel through SSH or using Stunnel
+sed -i '/\/bin\/false/d' /etc/shells
+sed -i '/\/usr\/sbin\/nologin/d' /etc/shells
+echo '/bin/false' >> /etc/shells
+echo '/usr/sbin/nologin' >> /etc/shells
+service ssh restart
+
+# Install Dropbear
+apt install -y dropbear
+# Removing some duplicate config file
+rm -rf /etc/default/dropbear*
+ 
+# creating dropbear config using cat eof tricks
+cat <<'MyDropbear' > /etc/default/dropbear
+# My Dropbear Config
+NO_START=0
+DROPBEAR_PORT=85
+DROPBEAR_EXTRA_ARGS="-p 86"
+DROPBEAR_BANNER="/etc/banner"
+DROPBEAR_RSAKEY="/etc/dropbear/dropbear_rsa_host_key"
+DROPBEAR_DSSKEY="/etc/dropbear/dropbear_dss_host_key"
+DROPBEAR_ECDSAKEY="/etc/dropbear/dropbear_ecdsa_host_key"
+DROPBEAR_RECEIVE_WINDOW=65536
+MyDropbear
+service dropbear restart
+
+# Install Stunnel
+apt install -y stunnel4
+StunnelDir=$(ls /etc/default | grep stunnel | head -n1)
+
+# Creating stunnel startup config using cat eof tricks
+cat <<'MyStunnelD' > /etc/default/$StunnelDir
+# My Stunnel Config
+ENABLED=1
+FILES="/etc/stunnel/*.conf"
+OPTIONS="/etc/banner"
+BANNER="/etc/banner"
+PPP_RESTART=0
+# RLIMITS="-n 4096 -d unlimited"
+RLIMITS=""
+MyStunnelD
+
+# Removing all stunnel folder contents
+rm -rf /etc/stunnel/*
+ 
+# Creating stunnel certifcate using openssl
+openssl req -new -x509 -days 9999 -nodes -subj "/C=MY/ST=NCR/L=Kuala_Lumpur/O=VoltNet/OU=VoltNet/CN=VoltNet" -out /etc/stunnel/stunnel.pem -keyout /etc/stunnel/stunnel.pem &> /dev/null
+##  > /dev/null 2>&1
+
+# Creating stunnel server config
+cat <<'MyStunnelC' > /etc/stunnel/stunnel.conf
+# My Stunnel Config
+pid = /var/run/stunnel.pid
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+TIMEOUTclose = 0
+[dropbear]
+accept = 441
+connect = 127.0.0.1:85
+[openssh]
+accept = 442
+connect = 127.0.0.1:225
+MyStunnelC
+
+service stunnel4 restart
+
+# Install Squid3
+apt install -y squid
+apt remove --purge squid -y
+wget "http://security.debian.org/debian-security/pool/updates/main/s/squid3/squid_3.5.23-5+deb9u7_amd64.deb" -qO squid.deb
+dpkg -i squid.deb
+rm -f squid.deb
+
+apt install libecap3 squid-common squid-langpack -y
+wget "http://security.debian.org/debian-security/pool/updates/main/s/squid3/squid_3.5.23-5+deb9u7_amd64.deb" -qO squid.deb
+dpkg -i squid.deb
+rm -f squid.deb
+
+cat <<mySquid > /etc/squid/squid.conf
+acl VPN dst $(wget -4qO- http://ipinfo.io/ip)
+http_access allow VPN
+http_access deny all 
+http_port 0.0.0.0:8000
+http_port 0.0.0.0:3128
+coredump_dir /var/spool/squid
+refresh_pattern ^ftp: 1440 20% 10080
+refresh_pattern ^gopher: 1440 0% 1440
+refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
+refresh_pattern . 0 20% 4320
+visible_hostname Volt
+mySquid
+service squid restart
+
+# Install OpenVPN
+
+# Install BadVPN UDPGw
+cd
+apt install -y cmake
+wget -qO badvpn.zip "https://raw.githubusercontent.com/iriszz-official/autoscript/main/FILES/badvpn.zip"
+unzip badvpn.zip
+cd badvpn-master
+mkdir build-badvpn
+cd build-badvpn
+cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
+make install
+cd
+rm -r badvpn-master
+rm badvpn.zip
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
+# Install Speedtest cli
+sudo apt-get install curl
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+sudo apt-get install speedtest
+
+# Install fail2ban
+apt install -y fail2ban
+service fail2ban restart
+
+# Install DDoS Deflate
+apt install -y dnsutils tcpdump dsniff grepcidr
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+# xml parser
+cd
+apt install -y libxml-parser-perl
+# blockir torrent
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+
+# Configure rc.local
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# Configure crontab
+echo "0 0 * * * root reboot" >> /etc/crontab
+netfilter-persistent save
+netfilter-persistent reload
+
+# Turning Off Multi-login Auto Kill
+rm -f /etc/cron.d/set_multilogin_autokill_lib
+# remove unnecessary files
+cd
+apt autoclean -y
+apt -y remove --purge unscd
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove bind9*;
+apt-get -y remove sendmail*
+apt autoremove -y
+# finishing
+cd
+/etc/init.d/openvpn restart
+/etc/init.d/cron restart
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/fail2ban restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/vnstat restart
+/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 4000
+history -c
+echo "unset HISTFILE" >> /etc/profile
+# finihsing
+clear
